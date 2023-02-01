a9ad8032ef03b060dc0cebc339aba7d50048a21b
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-01 17:56:06 +0100

    feat(web): update modal interface - more blur - transition for opening and closing modal - update content to fit new INotification interface data - better titles - link to event origin (post or story) - add more content to example notifications - replace hard-coded types with INotification

--- web/src/components/notification/Notification.tsx
+++ web/src/components/notification/Notification.tsx
@@ -1,19 +1,36 @@
-export default function Notification({
-	title,
-	author,
-	time,
-}: {
-	title: string;
-	author: string;
-	time: Date;
-}) {
+import { INotification } from "../../models/Notification";
+export default function Notification({ data }: { data: INotification }) {
+	// TODO: add icon for author
+	// TODO: add image for post
+	// TODO: add link to author
+	// TODO: add link to post
+	// TODO: format date: Days ago if not today, otherwise hours ago
+	let date = "";
+	let title = "";
+	switch (data.type) {
+		case "comment":
+			title = `commented on your post.`;
+			break;
+		case "likestory":
+			title = `liked your story.`;
+			break;
+		case "follow":
+			title = `followed you.`;
+			break;
+		case "likepost":
+			title = `liked your post.`;
+			break;
+		case "newpost":
+			title = `uploaded a new post.`;
+			break;
+	}
 	return (
-		<div className="flex flex-col p-2 border rounded w-full m-1">
-			<h3 className="text-lg">{title}</h3>
-			<p className="text-sm">{author}</p>
-			<span className="text-sm text-gray-500">
-				{time.toLocaleString()}
-			</span>
+		<div className="flex items-center justify-between p-2 border rounded w-full m-1">
+			<h3 className="text-lg">
+				<span className="font-bold">{data.author} </span>
+				{title}
+			</h3>
+			<span className="text-gray-500 ml-2">{date}</span>
 		</div>
 	);
 }
--- web/src/components/notification/NotificationModal.tsx
+++ web/src/components/notification/NotificationModal.tsx
@@ -2,20 +2,61 @@ import { XCircle } from "react-feather";
 import { useState, useEffect } from "react";
 
 import Notification from "./Notification";
+import { INotification } from "../../models/Notification";
 export default function NotificationModal({ closeNotificationModal }: any) {
 	const [notifications, setNotifications] = useState([]) as any;
 
 	useEffect(() => {
 		// TODO: Fetch notifications from API
 		setNotifications([
-			{ title: "Notification 1", author: "Trantlabs", time: new Date() },
-			{ title: "Notification 2", author: "Trantlabs", time: new Date() },
+			{
+				id: 0,
+				type: "likepost",
+				author: "Trantlabs",
+				link: "/post/0",
+				createdAt: new Date(),
+			},
+			{
+				id: 1,
+				type: "likestory",
+				author: "Trantlabs",
+				link: "/post/0",
+				createdAt: new Date(),
+			},
+			{
+				id: 2,
+				type: "comment",
+				message: "Great post",
+				author: "Trantlabs",
+				link: "/post/0",
+				createdAt: new Date(),
+			},
+			{
+				id: 3,
+				type: "follow",
+				author: "Trantlabs",
+				link: "/post/0",
+				createdAt: new Date(),
+			},
+			{
+				id: 4,
+				type: "newpost",
+				author: "Trantlabs",
+				link: "/post/0",
+				createdAt: new Date(),
+			},
 		]);
 	}, []);
 
 	return (
-		<div className="backdrop-blur-[2px] fixed z-1 top-0 left-0 min-w-full min-h-full flex justify-center items-center">
-			<div className="bg-white flex flex-col justify-center items-center rounded border p-4 w-1/2">
+		<div
+			className="backdrop-blur fixed z-1 top-0 left-0 min-w-full min-h-full flex justify-center items-center"
+			onClick={closeNotificationModal}
+		>
+			<div
+				className="bg-white flex flex-col justify-center items-center rounded border p-4 w-1/2"
+				onClick={(e) => e.stopPropagation()}
+			>
 				<div className="flex justify-between items-center w-full">
 					<h1 className="mr-2 text-xl">Notifications</h1>
 					<button onClick={closeNotificationModal}>
@@ -23,15 +64,9 @@ export default function NotificationModal({ closeNotificationModal }: any) {
 					</button>
 				</div>
 				<div className="flex justify-center items-center flex-col w-full p-4">
-					{notifications.map(
-						(notification: {
-							title: string;
-							author: string;
-							time: Date;
-						}) => (
-							<Notification {...notification} />
-						)
-					)}
+					{notifications.map((notification: INotification) => (
+						<Notification data={notification} />
+					))}
 				</div>
 			</div>
 		</div>
