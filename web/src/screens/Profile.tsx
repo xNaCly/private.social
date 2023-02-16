4c8e3a00f18cb28a974e4bdd4313b194b63b2fb8
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-16 08:32:17 +0100

    feat(web): finish profile heading

--- web/src/screens/Profile.tsx
+++ web/src/screens/Profile.tsx
@@ -1,10 +1,10 @@
 import { User } from "../models/User";
-import { Lock, Unlock, Settings, Edit, Share } from "react-feather";
+import { Settings, MapPin } from "react-feather";
 
 export default function Profile() {
 	const data: User = {
 		bio: {
-			text: "Hi, I’m Matteo, a 19 year old student from Germany. I am interested in programming, tech and hardware. In my freetime I like to contribute to free and open source software, as well as writing my own quality of life improving projects.",
+			text: "Hi, I’m Matteo, a 20 year old student from Germany.Hi, I’m Matteo, a 19 year old student from Germany.Hi, I’m Matteo, a 19 year old student from Germany.Hi, I’m Matteo, a 19 year old student from Germany.",
 			pronouns: "he/him",
 			location: "Germany",
 			website: "https://xnacly.me",
@@ -29,35 +29,54 @@ export default function Profile() {
 					src={data.avatar}
 					className="rounded-full w-40 cursor-pointer"
 				/>
-				<div className="ml-8 flex flex-col items-start grow">
+				<div className="ml-8 mt-2 flex flex-col items-start">
 					<div className="flex items-center">
-						<h3 className="text-lg">{data.display_name}</h3>
+						<h3 className="text-xl">{data.name}</h3>
 						<button className="hover:bg-gray-300 mx-3 px-3 py-1 flex justify-center rounded bg-gray-200 flex items-center transition-all">
 							Edit Profile
 						</button>
 						<button className="flex items-center justify-center">
-							<Settings size={16} className="" />
+							<Settings size={20} className="" />
 						</button>
 					</div>
-					<div className="flex my-8">
-						<div className="flex items-center">
+					<div className="flex my-4">
+						<div className="flex mr-4 items-center">
 							<h3 className="font-bold mr-1">
 								{data.stats.posts}
 							</h3>
 							<p>Posts</p>
 						</div>
-						<div className="flex mx-4 items-center">
+						<div className="flex mr-4 items-center">
 							<h3 className="font-bold mr-1">
 								{data.stats.followers}
 							</h3>
 							<p>Followers</p>
 						</div>
-						<div className="flex mx-4 items-center">
+						<div className="flex items-center">
 							<h3 className="font-bold mr-1">
 								{data.stats.following}
 							</h3>
 							<p>Following</p>
 						</div>
+					</div>
+					<div className="flex flex-col">
+						<p className="flex my-1 items-center">
+							<span className="mr-2">{data.display_name}</span>
+							<span className="mx-2 text-gray-400">
+								{data.bio.pronouns}
+							</span>
+							<div className="mx-2 flex items-center">
+								<MapPin size={14} className="mr-1" />
+								{data.bio.location}
+							</div>
+						</p>
+						<span className="my-1 max-w-xs">{data.bio.text}</span>
+						<a
+							className="my-1 text-trantlabs hover:text-gray-500"
+							href={data.bio.website}
+						>
+							{data.bio.website}
+						</a>
 					</div>
 				</div>
 			</div>
