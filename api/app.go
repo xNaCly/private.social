8f762a6c0f6c389f4fd103027ae237e1c461300d
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-22 17:34:51 +0100

    feat(api): implement basic database connection

--- api/app.go
+++ api/app.go
@@ -8,7 +8,10 @@ import (
 	"fmt"
 	"log"
 
+	"github.com/xnacly/private.social/api/config"
+	"github.com/xnacly/private.social/api/database"
 	"github.com/xnacly/private.social/api/handlers"
+	"github.com/xnacly/private.social/api/models"
 	"github.com/xnacly/private.social/api/router"
 	"github.com/xnacly/private.social/api/setup"
 	"github.com/xnacly/private.social/api/util"
@@ -27,6 +30,33 @@ var routes = []router.Route{
 
 func main() {
 	fmt.Print(util.ASCII_ART)
+
+	config.LoadDotEnv()
+
+	database.Db = database.Connect(config.Config["MONGO_URI"])
+
+	// TODO: remove test:
+	user := models.User{
+		Name:        "user",
+		DisplayName: "user",
+		Link:        "user",
+		Avatar:      "https://xnacly.me/avatar.png",
+		Private:     false,
+		CreatedAt:   util.GetTimeStamp(),
+		Bio: models.UserBio{
+			Text:     "this is the bio",
+			Pronouns: "they/them",
+			Location: "somewhere",
+			Website:  "https://xnacly.me",
+		},
+		Stats: models.UserStats{
+			Followers: 0,
+			Following: 0,
+			Posts:     0,
+		},
+	}
+	database.Db.InsertNewUser(user)
+	// -----------------
 
 	app := setup.Setup()
 	router.RegisterRoutes(app, "v1", routes...)
--- api/util/util.go
+++ api/util/util.go
@@ -1,5 +1,14 @@
 package util
 
+import (
+	"log"
+	"time"
+
+	"github.com/xnacly/private.social/api/config"
+
+	"go.mongodb.org/mongo-driver/bson/primitive"
+)
+
 const ASCII_ART = `
 ██████╗ ██████╗ ██╗██╗   ██╗ █████╗ ████████╗███████╗   ███████╗ ██████╗  ██████╗██╗ █████╗ ██╗                    █████╗ ██████╗ ██╗
 ██╔══██╗██╔══██╗██║██║   ██║██╔══██╗╚══██╔══╝██╔════╝   ██╔════╝██╔═══██╗██╔════╝██║██╔══██╗██║                   ██╔══██╗██╔══██╗██║
@@ -15,4 +24,15 @@ type ApiError struct {
 	Success bool   `json:"success"` // indicate if the request was successful or not
 	Code    int    `json:"code"`    // HTTP status code: 200, 400, 500, etc
 	Message string `json:"message"` // error text associated with the error
+}
+
+// debug logging, only prints if the PROD environment variable is set to false
+func DLog(msg string) {
+	if config.Config["PROD"] == "false" {
+		log.Println("[DEBUG]:", msg)
+	}
+}
+
+func GetTimeStamp() primitive.DateTime {
+	return primitive.NewDateTimeFromTime(time.Now())
 }
