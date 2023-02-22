6edb3ec827c7ee2cda4305af7ae8dbdf1d68b0cd
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-22 17:34:24 +0100

    feat(api): implement loading config from .env file

--- api/config/config.go
+++ api/config/config.go
@@ -1,16 +1,42 @@
 package config
 
 import (
-	"errors"
+	"bufio"
+	"log"
 	"os"
+	"strings"
 )
 
-func GetEnv(key string) (string, error) {
-	res := os.Getenv(key)
+// config dynamically loaded from .env file
+var Config map[string]string = make(map[string]string)
 
-	if res == "" {
-		return "", errors.New("env var not found")
+// loads key value pairs from a .env file into the Config map, .env must be in the same dir as the executable
+func LoadDotEnv() {
+	file, err := os.Open(".env")
+	defer file.Close()
+
+	if err != nil {
+		log.Fatal("failed to open .env ", err)
 	}
 
-	return res, nil
+	scanner := bufio.NewScanner(file)
+
+	for scanner.Scan() {
+		l := scanner.Text()
+
+		if len(l) == 0 || l[0] == '#' {
+			continue
+		}
+
+		nl := strings.Split(l, "=")
+
+		if len(nl) == 2 {
+			Config[nl[0]] = nl[1]
+			log.Printf("loaded variable '%s' with value '%s'\n", nl[0], nl[1])
+		}
+	}
+
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
+	}
 }
