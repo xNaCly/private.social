672a8c3d5d3f2e289c226a74caded57b5427734c
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-23 21:13:40 +0100

    test(api): add JWT_SECRET to config test

--- api/tests/config_test.go
+++ api/tests/config_test.go
@@ -8,8 +8,12 @@ import (
 
 func TestLoadConfig(t *testing.T) {
 	os.Setenv("MONGO_URL", "this is a test")
+	os.Setenv("JWT_SECRET", "this is a test")
 	config.LoadConfig()
 	if config.Config["MONGO_URL"] != "this is a test" {
+		t.Error("Failed to load config")
+	}
+	if config.Config["JWT_SECRET"] != "this is a test" {
 		t.Error("Failed to load config")
 	}
 }
