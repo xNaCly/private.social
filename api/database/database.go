19a724d7c3c74d482b6ba193d27d52d20f292275
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-23 08:01:20 +0100

    feat(api): error on InsertUser and ping to db

--- api/database/database.go
+++ api/database/database.go
@@ -8,7 +8,6 @@ import (
 	"go.mongodb.org/mongo-driver/mongo/options"
 
 	"github.com/xnacly/private.social/api/models"
-	"github.com/xnacly/private.social/api/util"
 )
 
 // Database is a wrapper for the mongo.Client struct
@@ -20,7 +19,7 @@ type Database struct {
 
 // establishes a connection to the database, failes if the connection cannot be established or the database is not reachable
 func Connect(db_string string) Database {
-	util.DLog("Establishing connection to database")
+	log.Println("Establishing connection to database")
 	if len(db_string) == 0 {
 		log.Fatal("Database string is empty, please provide a valid database string")
 	}
@@ -31,14 +30,17 @@ func Connect(db_string string) Database {
 		log.Fatalf("failed to establish database connection to '%s', error: %s", db_string, err)
 	}
 
-	if client.Ping(context.Background(), nil) != nil {
-		log.Fatalf("database is not reachable...")
+	err = client.Ping(context.Background(), nil)
+
+	if err != nil {
+		log.Fatalln("database is not reachable...: ", err)
 	}
 
-	util.DLog("Connection to database established")
+	log.Println("Connection to database established")
 	return Database{connection: client}
 }
 
-func (db Database) InsertNewUser(user models.User) {
-	db.connection.Database("ps").Collection("users").InsertOne(context.Background(), user)
+func (db Database) InsertNewUser(user models.User) error {
+	_, err := db.connection.Database("ps").Collection("users").InsertOne(context.Background(), user)
+	return err
 }
