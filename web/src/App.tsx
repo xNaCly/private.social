fadc4da5de3723a6400148705b7e6516d2e6be94
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-24 15:32:22 +0100

    feat(web): render app only if bearer is defined

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,8 +1,13 @@
 import Navigation from "./components/Navigation";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { useState, useEffect } from "react";
+
 import Error from "./screens/Error";
+import Signup from "./screens/Signup";
+import Login from "./screens/Login";
 import Profile from "./screens/Profile";
 import Home from "./screens/Home";
+
 import "./index.css";
 
 export default function App() {
@@ -19,15 +24,41 @@ export default function App() {
 │   The private, selfhostable and customizable social network - https://github.com/xnacly/private.social   │
 └──────────────────────────────────────────────────────────────────────────────────────────────────────────┘
 `);
+	const [bearer, setBearer] = useState<string | null>(null);
+
+	useEffect(() => {
+		let token = localStorage.getItem("bearer");
+		if (!token) setBearer(token);
+	}, []);
+
+	function updateBearer(bearer: string | null) {
+		setBearer(bearer);
+	}
+
 	return (
 		<Router>
-			<Navigation />
 			<div className="mx-6">
-				<Routes>
-					<Route index element={<Home />} />
-					<Route path="profile" element={<Profile />} />
-					<Route path="*" element={<Error />} />
-				</Routes>
+				{!bearer ? (
+					<Routes>
+						<Route
+							index
+							element={<Login bearerUpdater={updateBearer} />}
+						/>
+						<Route
+							path="/signup"
+							element={<Signup bearerUpdater={updateBearer} />}
+						/>
+					</Routes>
+				) : (
+					<>
+						<Navigation />
+						<Routes>
+							<Route index element={<Home />} />
+							<Route path="profile" element={<Profile />} />
+							<Route path="*" element={<Error />} />
+						</Routes>
+					</>
+				)}
 			</div>
 		</Router>
 	);
