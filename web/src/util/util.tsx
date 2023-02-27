f14cde56545a9255cbd7f282a92a333633b47c9e
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-27 10:26:21 +0100

    feat(web): check if backend ping success

--- web/src/util/util.tsx
+++ web/src/util/util.tsx
@@ -1,7 +1,17 @@
+import { xfetch } from "./fetch";
+
 export function getToken(): string | null {
 	return localStorage.getItem("bearer");
 }
 
-export function setToken(token: string){
-    localStorage.setItem("bearer", token)
+export function setToken(token: string) {
+	localStorage.setItem("bearer", token);
+}
+
+export async function isBackendAvailable(): Promise<boolean> {
+	try {
+		return (await xfetch("/ping", {})).success;
+	} catch {
+		return false;
+	}
 }
