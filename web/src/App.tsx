f00f50421751824a0693cca48b6b7ca388e40fe0
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-27 10:28:00 +0100

    feat(web): check if backend is available in app

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -6,7 +6,7 @@ import {
 	Navigate,
 } from "react-router-dom";
 import { useState } from "react";
-import { getToken } from "./util/util";
+import { getToken, isBackendAvailable } from "./util/util";
 
 import Error from "./screens/Error";
 import Signup from "./screens/Signup";
@@ -16,6 +16,9 @@ import Home from "./screens/Home";
 
 import "./index.css";
 
+let backendAvailable = await isBackendAvailable();
+console.log(backendAvailable);
+
 export default function App() {
 	const [bearer, setBearer] = useState<string | null>(getToken());
 
@@ -24,41 +27,63 @@ export default function App() {
 	}
 
 	return (
-		<Router>
-			<div className="mx-6">
-				{bearer && <Navigation />}
-				<Routes>
-					{!bearer ? (
-						<>
-							<Route
-								index
-								element={<Login bearerUpdater={updateBearer} />}
-							/>
-							<Route
-								path="/signup"
-								element={
-									<Signup bearerUpdater={updateBearer} />
-								}
-							/>
-							<Route path="*" element={<Error />} />
-						</>
-					) : (
-						<>
-							<Route index element={<Home />} />
-							<Route path="profile" element={<Profile />} />
-							<Route
-								path="login"
-								element={<Navigate to="/" replace />}
-							/>
-							<Route
-								path="signup"
-								element={<Navigate to="/" replace />}
-							/>
-							<Route path="*" element={<Error />} />
-						</>
-					)}
-				</Routes>
-			</div>
-		</Router>
+		<>
+			{backendAvailable ? (
+				<Router>
+					<div className="mx-6">
+						{bearer && <Navigation />}
+						<Routes>
+							{!bearer ? (
+								<>
+									<Route
+										index
+										element={
+											<Login
+												bearerUpdater={updateBearer}
+											/>
+										}
+									/>
+									<Route
+										path="/signup"
+										element={
+											<Signup
+												bearerUpdater={updateBearer}
+											/>
+										}
+									/>
+									<Route path="*" element={<Error />} />
+								</>
+							) : (
+								<>
+									<Route index element={<Home />} />
+									<Route
+										path="profile"
+										element={<Profile />}
+									/>
+									<Route
+										path="login"
+										element={<Navigate to="/" replace />}
+									/>
+									<Route
+										path="signup"
+										element={<Navigate to="/" replace />}
+									/>
+									<Route path="*" element={<Error />} />
+								</>
+							)}
+						</Routes>
+					</div>
+				</Router>
+			) : (
+				<>
+					<div className="flex flex-col items-center justify-center h-screen">
+						<h1 className="text-4xl font-bold">
+							Backend is not available, the instance hoster did
+							not configure private.social correctly!
+						</h1>
+					</div>
+				</>
+			)}
+		</>
 	);
 }
