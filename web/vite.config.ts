3191e7c886b86701bc228b469f913d3a1a89d051
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-02-28 16:54:14 +0100

    feat(web): move to vite proxy

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,10 +1,20 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
 	server: {
 		port: 3000,
+		proxy: {
+			"/api": {
+				target: "http://localhost:8000/",
+				secure: false,
+				configure: (proxy, _options) => {
+					proxy.on("error", (err, _req, _res) => {
+						console.log("proxy error", err, _req.url);
+					});
+				},
+			},
+		},
 	},
 });
