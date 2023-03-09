c306a5557eb2cf2057c23d58042d7c0610c6e88a
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-09 16:27:15 +0100

    feat(api): create and get posts

---
+++ api/handlers/post.go
@@ -0,0 +1,90 @@
+package handlers
+
+import (
+	"github.com/xnacly/private.social/api/database"
+	"github.com/xnacly/private.social/api/models"
+	"github.com/xnacly/private.social/api/util"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+func CreatePost(c *fiber.Ctx) error {
+	incomingPost := new(models.CreatePost)
+
+	if err := c.BodyParser(incomingPost); err != nil {
+		return fiber.NewError(fiber.StatusBadRequest, "Invalid post data")
+	}
+
+	log.Println(incomingPost)
+	if len(incomingPost.Url) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Url is required")
+	} else if len(incomingPost.Description) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Description is required")
+	}
+
+	user := c.Locals("dbUser").(models.User)
+	post := models.Post{
+		Url:           incomingPost.Url,
+		Description:   incomingPost.Description,
+		Author:        user.Id,
+		CreatedAt:     util.GetTimeStamp(),
+		LikeAmount:    0,
+		CommentAmount: 0,
+	}
+
+	postid, err := database.Db.InsertNewPost(post)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Something went wrong while creating the post")
+	}
+
+	return c.Status(fiber.StatusCreated).JSON(util.ApiResponse{
+		Code:    fiber.StatusCreated,
+		Success: true,
+		Data:    fiber.Map{"post_id": postid},
+		Message: "Post created successfully",
+	})
+}
+
+func GetPosts(c *fiber.Ctx) error {
+	user := c.Locals("dbUser").(models.User)
+	posts, err := database.Db.GetAllPostsByUserId(user.Id)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Something went wrong while fetching the posts")
+	}
+
+	return c.Status(fiber.StatusOK).JSON(util.ApiResponse{
+		Code:    fiber.StatusOK,
+		Success: true,
+		Data:    fiber.Map{"posts": posts},
+		Message: "Posts fetched successfully",
+	})
+}
+
+// TODO: does not delete successfully, i really fucking hate mongodb
+func DeletePost(c *fiber.Ctx) error {
+	user := c.Locals("dbUser").(models.User)
+	postid := c.Params("id")
+
+	if len(postid) == 0 {
+		return fiber.NewError(fiber.StatusBadRequest, "Post id is required")
+	}
+
+	if !database.Db.DoesPostExist(postid) {
+		return fiber.NewError(fiber.StatusNotFound, "Post not found")
+	}
+
+	err := database.Db.DeletePostById(postid, user.Id)
+
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "Something went wrong while deleting the post")
+	}
+
+	return c.JSON(util.ApiResponse{
+		Success: true,
+		Code:    fiber.StatusOK,
+		Message: "Post deleted successfully",
+	})
+}
