02030d55a5cc9b11b0870d29227753d25e6f1a32
xnacly <47723417+xNaCly@users.noreply.github.com>
2023-03-15 19:45:43 +0100

    refactor(web): make error icon red

--- web/src/screens/Error.tsx
+++ web/src/screens/Error.tsx
@@ -3,7 +3,7 @@ export default function Error() {
 	return (
 		<div className="h-screen w-screen flex justify-center items-center">
 			<div className="flex justify-center items-center flex-col">
-				<AlertCircle className="mb-2" size={58} />
+				<AlertCircle className="mb-2 text-red-500" size={58} />
 				<h1 className="text-9xl font-bold m-4">404</h1>
 				<p className="text-lg italic">page not found</p>
 			</div>
